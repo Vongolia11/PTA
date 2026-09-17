28c18f40d38ab1f4b54e29093cba119418820803
Vongolia11 <wpcmct@gmail.com>
2026-09-17 10:52:12 +0200

    Add files via upload

--- HAR/HAR_Task.py
+++ HAR/HAR_Task.py
@@ -3,23 +3,35 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import numpy as np
 
-from NewTask import NewTask
+# NOTE:
+# The original supplementary package referenced `NewTask`, but that file was
+# omitted during later code reorganization. The public release uses the
+# available Task implementation.
+from Task import Task
 from XRF55_Dataset import XRF55_Dataset
 from DualNet import *
 from Encoders import *
 from engine import *
-from Extractor import mmwave_feature_extractor, wifi_feature_extractor, rfid_feature_extractor, classification_Head
+from Extractor import (
+    mmwave_feature_extractor,
+    wifi_feature_extractor,
+    rfid_feature_extractor,
+    classification_Head,
+)
 from misc import *
 from backbone_models.mmWave.ResNet import *
 
-class HAR_Task(NewTask):
+
+class HAR_Task(Task):
     def __init__(self):
         super().__init__(
             task_name="xrf55_har",
-            modalities=["mmwave", "wifi", "rfid"]
+            modalities=["mmwave", "wifi", "rfid"],
         )
 
     def set_encoders(self):
@@ -32,14 +44,33 @@ class HAR_Task(NewTask):
         rfid_model = torch.load('./backbone_models/RFID/rfid_ResNet18.pt')
         rfid_extractor = rfid_feature_extractor(rfid_model).eval()
 
-        encoders_list = [mmwave_extractor, wifi_extractor, rfid_extractor]
-        encode_info = [(512, 512, 32), (512, 512, 4), (512, 512, 5)]
+        # `.eval()` above does not freeze parameters. Task.train() subsequently
+        # calls model.train(), and these encoder parameters remain in the main
+        # optimizer. Thus the released HAR pipeline fine-tunes the encoders.
+        encoders_list = [
+            mmwave_extractor,
+            wifi_extractor,
+            rfid_extractor,
+        ]
+
+        encode_info = [
+            (512, 512, 32),
+            (512, 512, 4),
+            (512, 512, 5),
+        ]
 
         for i, name in enumerate(self.modalities):
-            self.encoders[name] = Encoder(i, encoders_list[i], encode_info[i])
+            self.encoders[name] = Encoder(
+                i,
+                encoders_list[i],
+                encode_info[i],
+            )
 
     def set_decoder(self):
-        self.decoder = classification_Head(emb_size=512, num_classes=55)
+        self.decoder = classification_Head(
+            emb_size=512,
+            num_classes=55,
+        )
 
     def set_losses(self):
         self.losses["ce"] = nn.CrossEntropyLoss()
@@ -48,7 +79,7 @@ class HAR_Task(NewTask):
         full_dataset = XRF55_Dataset(
             root_dir="./Data/Split_XRF55_Dataset",
             is_train=True,
-            split='train'
+            split="train",
         )
 
         if not full_dataset:
@@ -58,40 +89,60 @@ class HAR_Task(NewTask):
         val_size = int(val_split_ratio * num_samples)
         train_size = num_samples - val_size
 
+        # Fixed split for the inner-loop / outer-loop subsets.
         generator = torch.Generator().manual_seed(42)
-        train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size], generator=generator)
+        train_dataset, val_dataset = random_split(
+            full_dataset,
+            [train_size, val_size],
+            generator=generator,
+        )
 
-        train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn_padd)
-        val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False, collate_fn=collate_fn_padd)
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=32,
+            shuffle=True,
+            collate_fn=collate_fn_padd,
+        )
+        val_dataloader = DataLoader(
+            val_dataset,
+            batch_size=16,
+            shuffle=False,
+            collate_fn=collate_fn_padd,
+        )
 
         return train_dataloader, val_dataloader
 
     def set_test_data(self, args):
-        test_dataset = XRF55_Dataset_new(
+        # Packaging fix: the released repository contains XRF55_Dataset rather
+        # than the stale XRF55_Dataset_new identifier used in an older script.
+        test_dataset = XRF55_Dataset(
             root_dir=args.data_dir,
             is_train=False,
-            split='test'
+            split="test",
         )
 
-        test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
-                                     collate_fn=collate_fn_padd)
+        test_dataloader = DataLoader(
+            test_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            collate_fn=collate_fn_padd,
+        )
         return test_dataloader
 
+
 def collate_fn_padd(batch):
-    '''
-    Padds batch of variable length
-    '''
+    """Collate XRF55 modality arrays and labels into tensors."""
     labels = []
     [labels.append(float(t[3])) for t in batch]
     labels = torch.FloatTensor(labels)
 
-    mmwave_data = np.array([(t[2]) for t in batch])
+    mmwave_data = np.array([t[2] for t in batch])
     mmwave_data = torch.FloatTensor(mmwave_data)
 
-    wifi_data = np.array([(t[0]) for t in batch])
+    wifi_data = np.array([t[0] for t in batch])
     wifi_data = torch.FloatTensor(wifi_data)
 
-    rfid_data = np.array([(t[1]) for t in batch])
+    rfid_data = np.array([t[1] for t in batch])
     rfid_data = torch.FloatTensor(rfid_data)
 
     return mmwave_data, wifi_data, rfid_data, labels
